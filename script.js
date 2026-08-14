e9bd62a8ef6edf7bba2fd6430d9422b88025f0b2
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-14 08:35:02 +0600

    explore guss my number

--- script.js
+++ script.js
@@ -4,27 +4,18 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 20;
 
+// const number = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.number').textContent = number;
+// document.querySelector('.check').addEventListener('click', function () {
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+//   // When there is no input
+//   if (!guess) {
+//     document.querySelector('.message').textContent = '⛔ No number!';
 
-  // When there is no input
-  if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number!';
+//     // When player wins
+//   }
+// });
 
-    // When player wins
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
-    document.querySelector('.number').textContent = secretNumber;
-
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-
-    // When guess is wrong
-  }});
+//Modal window
